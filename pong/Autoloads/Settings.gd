14976bd8c2f3ffe598aafe92b913407e13a0cb18
Rido <daniridao@hotmail.com>
2019-10-14 20:11:06 +0100

    Now audio settings work

--- pong/Autoloads/Settings.gd
+++ pong/Autoloads/Settings.gd
@@ -14,22 +14,24 @@ var musicVolume : float = 50.0 setget setMusicVolume, getMusicVolume
 
 var gameMode : String = GAME_MODES.single
 
-func adjustVolume(volume : float) -> float:
+func adjustVolume(volume : float, bus : String) -> float:
 	if volume > 100:
 		volume = 100
 	elif volume < 0:
 		volume = 0
+	var adjustedBusVolume = (volume * 0.8) - 70.0
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), adjustedBusVolume)
 	return volume
 
 func setSoundVolume(volume : float) -> void:
-	soundVolume = adjustVolume(volume)
+	soundVolume = adjustVolume(volume, "Sounds")
 	emit_signal("soundVolumeChange", soundVolume)
 	
 func getSoundVolume() -> float:
 	return soundVolume
 	
 func setMusicVolume(volume : float) -> void:
-	musicVolume = adjustVolume(volume)
+	musicVolume = adjustVolume(volume, "Music")
 	emit_signal("musicVolumeChange", musicVolume)
 	
 func getMusicVolume() -> float:
